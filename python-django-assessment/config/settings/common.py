6cb1aa930f8de9e25eabdc1b220767bf1097fd36
Camilo Sosa <camilososa98@gmail.com>
2021-03-22 11:05:12 -0300

    Requeriments

--- python-django-assessment/config/settings/common.py
+++ python-django-assessment/config/settings/common.py
@@ -44,6 +44,7 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.WhiteNoiseMiddleware',
 )
 
 MIGRATION_MODULES = {
